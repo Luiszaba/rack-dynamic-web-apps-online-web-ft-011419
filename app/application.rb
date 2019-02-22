25b220dc2468a675bc6878631abfe0ef9f51d82c
Luis Zabala <luis.zabala076@gmail.com>
2019-02-22 22:15:00 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,8 +3,8 @@ class Application
   def call(env)
     resp = Rack::Response.new
     num1 = Kernel.rand(1..20)
-    num2 = Kernal.rand(1..20)
-    num3 = Kernal.rand(1..20)
+    num2 = Kernel.rand(1..20)
+    num3 = Kernel.rand(1..20)
     
     
     resp.write "#{num1}\n"
